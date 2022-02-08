0a657aaf10577be0c133bb5d772ce23800efdc00
Brandon-Magofna <magofna68@gmail.com>
2022-02-08 13:41:25 -0800

    update UI for li styling

--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -4,22 +4,17 @@ import SearchIcon from '@mui/icons-material/Search';
 const SearchBar = (props) => {
   const [ term, setTerm ] = useState("");
   const searchInput = React.useRef(null);
+
   useEffect(()=> {
     searchInput.current.focus();
   }, [term]);
-
-    // function handleChange (e) {
-    //   const lastSearchedTerm = {...term};
       
     const handleChange = (e) => {
       props.onSearchTermChange(e.target.value);
       const searchedTerm = e.target.value
       const newTerm = {...searchedTerm}
-      setTerm(...searchedTerm);
-      console.log({term});
-  
-  
-    setTerm(newTerm);
+      setTerm(newTerm);
+      console.log({term});  
   }
 
   return (
