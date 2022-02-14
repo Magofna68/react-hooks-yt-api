a28a49171ab6a24ff3d57e0f129a283b4d7701bc
Brandon-Magofna <magofna68@gmail.com>
2022-02-14 10:15:42 -0800

    Updates

---
+++ precache-manifest.f16c57a2af005c6502c602281f9c3b96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "410e3714136932eaf8d31e5127bfc9d5",
+    "url": "/react-hooks-yt-api/index.html"
+  },
+  {
+    "revision": "9e82e55b1d16b48fc779",
+    "url": "/react-hooks-yt-api/static/css/main.57d33dc4.chunk.css"
+  },
+  {
+    "revision": "5ffbee15545cc3c962b3",
+    "url": "/react-hooks-yt-api/static/js/2.9850f077.chunk.js"
+  },
+  {
+    "revision": "5f5a2437a5d1efa8c90937d119e5d662",
+    "url": "/react-hooks-yt-api/static/js/2.9850f077.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e82e55b1d16b48fc779",
+    "url": "/react-hooks-yt-api/static/js/main.b717e680.chunk.js"
+  },
+  {
+    "revision": "418b88b8ad0dc63cb59c",
+    "url": "/react-hooks-yt-api/static/js/runtime-main.67f28fdc.js"
+  }
+]);
